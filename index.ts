3cee4ed224b0e80f24f67169ce1b480295a160fd
Joseph Morag <jm@josephmorag.com>
2020-10-16 16:34:41 -0400

    Handle command line args with yargs

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as ts from 'typescript'
 import * as fs from 'fs'
+import * as yargs from 'yargs'
 
 // taken from https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
 function check(fileNames: string[], options: ts.CompilerOptions): void {
@@ -24,13 +25,27 @@ function check(fileNames: string[], options: ts.CompilerOptions): void {
   })
   process.exit(count === 0 ? 0 : 1)
 }
+
+const readConfig = (path: string): ts.CompilerOptions => {
+  const rawConfig = ts.readConfigFile(path, filename =>
+    fs.readFileSync(filename, 'utf8')
+  ).config
+  return (
+    ts.convertCompilerOptionsFromJson(rawConfig?.compilerOptions, '.')
+      .options || {}
+  )
 }
 
-const tsconfig = ts.readConfigFile('tsconfig.json', filename =>
-  fs.readFileSync(filename, 'utf8')
-)
+const argv = yargs(process.argv.slice(2))
+  .demandCommand(1)
+  .default('tsconfig', './tsconfig.json')
+  .default('tsconfig-paths', './tsconfig.paths.json')
+  .usage('Usage: $0 src/components/*.tsx').argv
 
-compile(process.argv.slice(2), {
-  ...tsconfig.config,
+const options = {
+  ...readConfig(argv['tsconfig-paths']),
+  ...readConfig(argv.tsconfig),
   noEmit: true,
-})
+}
+
+check(argv._, options)
