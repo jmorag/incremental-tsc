5644c7bd12750fe41d84d54257694946f319fbc1
Joseph Morag <jm@josephmorag.com>
2020-10-30 12:37:35 -0400

    Allow specification of line ranges on command line args

--- index.ts
+++ index.ts
@@ -2,9 +2,15 @@
 import * as ts from 'typescript'
 import * as fs from 'fs'
 import * as yargs from 'yargs'
+// This represents the + section of the @@ header of a patch
+interface File {
+  fileName: string
+  lines?: { start: number; extent: number }
+}
 
 // taken from https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
-function check(fileNames: string[], options: ts.CompilerOptions): void {
+function check(files: File[], options: ts.CompilerOptions): void {
+  const fileNames = files.map(f => f.fileName)
   const program = ts.createProgram(fileNames, options)
   const emitResult = program.emit()
 
@@ -12,19 +18,23 @@ function check(fileNames: string[], options: ts.CompilerOptions): void {
     .getPreEmitDiagnostics(program)
     .concat(emitResult.diagnostics)
 
-  let count = 0
+  let exitCode = 0
   allDiagnostics.forEach((diagnostic: ts.Diagnostic) => {
     const fileName = diagnostic.file.fileName
     if (diagnostic.file && fileNames.includes(fileName)) {
       const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
         diagnostic.start!
       )
-      const message = getMessageText(diagnostic)
-      console.log(`${fileName} (${line + 1},${character + 1}): ${message}`)
-      count++
+      const lines = files.find(f => f.fileName === fileName).lines
+      const abs = (x: number) => x >= 0 ? x : -x
+      if (lines === undefined || abs(line + 1 - lines.start) <= lines.extent) {
+        const message = getMessageText(diagnostic)
+        console.log(`${fileName} (${line + 1},${character + 1}): ${message}`)
+        exitCode = 1
+      }
     }
   })
-  process.exit(count === 0 ? 0 : 1)
+  process.exit(exitCode)
 }
 
 function getMessageText(diagnostic: ts.Diagnostic): string {
@@ -46,6 +56,26 @@ function readConfig(path: string): ts.CompilerOptions {
   )
 }
 
+function parseArgs(args: string[]): File[] {
+  return args.map(arg => {
+    const colon_ix = arg.indexOf(':')
+    if (colon_ix === -1) return { fileName: arg }
+
+    const errorMessage = `malformed argument ${arg}. Should be in the form file.ts:1,10`
+    const fileName = arg.substring(0, colon_ix)
+    const comma_ix = arg.indexOf(',')
+    if (comma_ix === -1) throw errorMessage
+
+    const start = parseInt(arg.substring(colon_ix + 1, comma_ix), 10)
+    if (isNaN(start)) throw errorMessage
+
+    const extent = parseInt(arg.substring(comma_ix + 1), 10)
+    if (isNaN(extent)) throw errorMessage
+
+    return { fileName, lines: { start, extent } }
+  })
+}
+
 const argv = yargs(process.argv.slice(2))
   .demandCommand(1)
   .default('tsconfig', './tsconfig.json')
@@ -58,4 +88,4 @@ const options = {
   noEmit: true,
 }
 
-check(argv._, options)
+check(parseArgs(argv._), options)
